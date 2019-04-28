d67eb3a3f33f38109da3a1439c28a83c253d761d
Lisa <43095680+wabbitgurl@users.noreply.github.com>
2019-04-27 20:59:19 -0400

    Add files via upload

---
+++ MonthlyLoanPaymentCalculator/MonthlyLoanPaymentCalculator/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MonthlyLoanPaymentCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPurchasePrice = new System.Windows.Forms.TextBox();
+            this.txtDownPaymentAmount = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtMonthlyPayment = new System.Windows.Forms.TextBox();
+            this.txtAmountToFinance = new System.Windows.Forms.TextBox();
+            this.txtLoanTerm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPurchasePrice
+            // 
+            this.txtPurchasePrice.BackColor = System.Drawing.Color.Khaki;
+            this.txtPurchasePrice.Location = new System.Drawing.Point(187, 21);
+            this.txtPurchasePrice.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPurchasePrice.Name = "txtPurchasePrice";
+            this.txtPurchasePrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPurchasePrice.TabIndex = 0;
+            // 
+            // txtDownPaymentAmount
+            // 
+            this.txtDownPaymentAmount.BackColor = System.Drawing.Color.Khaki;
+            this.txtDownPaymentAmount.Location = new System.Drawing.Point(187, 93);
+            this.txtDownPaymentAmount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDownPaymentAmount.Name = "txtDownPaymentAmount";
+            this.txtDownPaymentAmount.Size = new System.Drawing.Size(100, 20);
+            this.txtDownPaymentAmount.TabIndex = 1;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.BackColor = System.Drawing.Color.Khaki;
+            this.txtInterestRate.Location = new System.Drawing.Point(187, 166);
+            this.txtInterestRate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(100, 20);
+            this.txtInterestRate.TabIndex = 2;
+            // 
+            // txtMonthlyPayment
+            // 
+            this.txtMonthlyPayment.BackColor = System.Drawing.Color.Silver;
+            this.txtMonthlyPayment.Location = new System.Drawing.Point(477, 165);
+            this.txtMonthlyPayment.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMonthlyPayment.Name = "txtMonthlyPayment";
+            this.txtMonthlyPayment.ReadOnly = true;
+            this.txtMonthlyPayment.Size = new System.Drawing.Size(116, 20);
+            this.txtMonthlyPayment.TabIndex = 5;
+            // 
+            // txtAmountToFinance
+            // 
+            this.txtAmountToFinance.BackColor = System.Drawing.Color.Silver;
+            this.txtAmountToFinance.Location = new System.Drawing.Point(477, 92);
+            this.txtAmountToFinance.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAmountToFinance.Name = "txtAmountToFinance";
+            this.txtAmountToFinance.ReadOnly = true;
+            this.txtAmountToFinance.Size = new System.Drawing.Size(116, 20);
+            this.txtAmountToFinance.TabIndex = 4;
+            // 
+            // txtLoanTerm
+            // 
+            this.txtLoanTerm.BackColor = System.Drawing.Color.Khaki;
+            this.txtLoanTerm.Location = new System.Drawing.Point(477, 20);
+            this.txtLoanTerm.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtLoanTerm.Name = "txtLoanTerm";
+            this.txtLoanTerm.Size = new System.Drawing.Size(116, 20);
+            this.txtLoanTerm.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Purchase Price";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lavender;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Down Payment Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lavender;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Interest Rate (annual)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lavender;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(334, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Monthly Payment";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lavender;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(334, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Amount To Finance";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Lavender;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(334, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Loan Term (Months)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Khaki;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(31, 225);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 22);
+            this.btnCalculate.TabIndex = 12;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Khaki;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(511, 225);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 22);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(603, 265);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMonthlyPayment);
+            this.Controls.Add(this.txtAmountToFinance);
+            this.Controls.Add(this.txtLoanTerm);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtDownPaymentAmount);
+            this.Controls.Add(this.txtPurchasePrice);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Monthly Loan Payment Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPurchasePrice;
+        private System.Windows.Forms.TextBox txtDownPaymentAmount;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtMonthlyPayment;
+        private System.Windows.Forms.TextBox txtAmountToFinance;
+        private System.Windows.Forms.TextBox txtLoanTerm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ MonthlyLoanPaymentCalculator/MonthlyLoanPaymentCalculator/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MonthlyLoanPaymentCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /*Conversion of strings to numeric values using TryParse...*/
+            string purchasePrice = txtPurchasePrice.Text;
+            string downPymt = DownPayment();
+            string interest = Interest();
+            string term = LoanTerm();
+
+            double.TryParse(purchasePrice, out double userLoanAmt);
+            double.TryParse(downPymt, out double userDownPymt);
+            double.TryParse(interest, out double userInterest);
+            int.TryParse(term, out int userTerm);
+
+            //Variables declared...
+            double financeAmt = userLoanAmt - userDownPymt;
+            double monthlyPymt;
+            double monthlyRate = ((userInterest / 12) / 100);
+
+        //Checking for invalid or absent input...
+        if (txtPurchasePrice.Text == "" || !double.TryParse(purchasePrice, out userLoanAmt))
+        {
+            txtPurchasePrice.BackColor = Color.Red;
+            MessageBox.Show("INVALID OR EMPTY FIELD! PLEASE TRY AGAIN!");
+        }
+        else if (txtInterestRate.Text == "" || !double.TryParse(interest, out userInterest))
+        {
+            txtInterestRate.BackColor = Color.Red;
+            MessageBox.Show("INVALID OR EMPTY FIELD! PLEASE TRY AGAIN!");
+        }
+        else if (txtLoanTerm.Text == "" || !int.TryParse(term, out userTerm))
+        {
+            txtLoanTerm.BackColor = Color.Red;
+            MessageBox.Show("INVALID OR EMPTY FIELD! PLEASE TRY AGAIN!");
+        }
+
+         monthlyPymt = (monthlyRate * financeAmt) / (1 - Math.Pow(1 + monthlyRate, userTerm * -1));
+
+        /*Converting numeric values to string values for output
+        and formatting output to currency...*/
+        txtAmountToFinance.Text = financeAmt.ToString("c2");
+        txtMonthlyPayment.Text = monthlyPymt.ToString("c2");
+    }
+
+        /*Just thought I'd give a few simple methods a try..
+         They're not too much to write home about, but just
+         gave it a whirl!*/
+
+        private string DownPayment()
+        {
+            return txtDownPaymentAmount.Text;
+        }
+        private string Interest()
+        {
+            return txtInterestRate.Text;
+        }
+        private string LoanTerm()
+        {
+            return txtLoanTerm.Text;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for stopping by! Have a great day!");
+            Application.Exit();
+        }
+
+        //Okay...this is a whoopsie and I'm not messing with it right now!
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
